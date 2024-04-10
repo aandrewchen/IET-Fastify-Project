65ad1255ba72c9eebda6a557e47236f251365075
aandrewchen <andche@ucdavis.edu>
2024-04-10 11:42:58 -0700

    Implemented routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,10 +1,21 @@
 import userController from '../controllers/user.controller.js';
 
+const UserSchema = {
+    type: 'object',
+    required: ['firstName', 'lastName', 'email'],
+    properties: {
+        firstName: { type: 'string' },
+        lastName: { type: 'string' },
+        email: { type: 'string', format: 'email' },
+        role: { type: 'string', enum: ['Admin', 'Project manager', 'Team member'], default: 'Team member' },
+    },
+};
+
 async function routes(fastify, options, done) {
     fastify.get("/", userController.getAllUsers);
     fastify.get("/:id", userController.getUserById);
-    fastify.post("/", userController.createUser);
-    fastify.put("/:id", userController.updateUser);
+    fastify.post("/", { schema: UserSchema }, userController.createUser);
+    fastify.put("/:id", { schema: UserSchema }, userController.updateUser);
     fastify.delete("/:id", userController.deleteUser);
 
     done();
